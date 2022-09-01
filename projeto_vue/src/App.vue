20ece9a82fb98c2cbcee20271b14e5c68a98a123
MatheusOsterno <matheusosternodev@gmail.com>
2022-08-31 23:19:22 -0300

    Novas atualizaçoes

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PrimeiroComponente />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PrimeiroComponente from '../src/components/PrimeiroComponente.vue'
 export default {
+  components: { PrimeiroComponente },
   name: 'App',
-  components: {
-    HelloWorld
+  Scomponents: {
+    PrimeiroComponente
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
